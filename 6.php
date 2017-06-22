d871ac49abb030bf1640f86010e80884fcc201a3
Andrew <andrey.albul@gmail.com>
2017-06-23 02:34:02 +0300

    Расширьте конструкцию if из п.5, выводя фразу: "Вам пора на пенсию" при условии, что значение переменной age больше 59.

---
+++ 6.php
@@ -0,0 +1,13 @@
+<?php
+$age = NULL;
+if($age >= 18 && $age <= 59){
+    echo "Вам еще работать и работать";
+}   elseif ($age > 59){
+    echo "Вам пора на пенсию";
+}
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 23.06.2017
+ * Time: 2:23
+ */
