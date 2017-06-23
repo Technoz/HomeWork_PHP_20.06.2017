eb63ee99e6c31108ccb4f2b6e62f218e7640246f
Andrew <andrey.albul@gmail.com>
2017-06-23 05:28:25 +0300

    21. Приведите число 0 к типу boolean. Объясните результат.</p>

---
+++ 21.php
@@ -0,0 +1,10 @@
+<?php
+$a = 0;
+$b = (bool)$a;
+var_dump($b); // При приведении числа к булевуму типу, 0 даст false;
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 23.06.2017
+ * Time: 5:26
+ */
