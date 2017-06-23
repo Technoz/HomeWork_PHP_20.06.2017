7db5eada8611cda3ff92a34951d3ba24272c2696
Andrew <andrey.albul@gmail.com>
2017-06-23 05:16:02 +0300

    Создайте две переменные $a = '78' (строковый тип) и $b = 78 (целочисленный тип). Проверьте эти две переменные на равность, с помощью if выведите "равны" или "не равны".

---
+++ 17.php
@@ -0,0 +1,14 @@
+<?php
+$a = '78';
+$b = 78;
+if($a == $b){
+    echo "Равны";
+}   else{
+    echo "Не равны";
+}
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 23.06.2017
+ * Time: 5:12
+ */
