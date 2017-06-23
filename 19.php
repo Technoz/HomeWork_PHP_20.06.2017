2cdc08d2c8e4aee2ccd15d7c449bedbb959e30a5
Andrew <andrey.albul@gmail.com>
2017-06-23 05:21:04 +0300

    Выведите результат сравнения $a и $b из п.17 с помощью var_dump

---
+++ 19.php
@@ -0,0 +1,14 @@
+<?php
+$a = '78';
+$b = 78;
+if($a == $b){
+    echo var_dump($a, $b) . " - Равны";
+}   else{
+    echo "Не равны";
+}
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 23.06.2017
+ * Time: 5:19
+ */
