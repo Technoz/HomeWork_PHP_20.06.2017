abf5b50131b81f620cc0ab366c93ef72d70a0f84
Andrew <andrey.albul@gmail.com>
2017-06-23 12:34:53 +0300

    Исправил ошибку: не проверялась $b на 0 и $operator на '%'

--- 15.php
+++ 15.php
@@ -1,24 +1,26 @@
 <?php
-$a = -10;
-$b = 2;
-$operator = '%';
-if($b == 0 && $operator == '/'){
+$a = 5;
+$b = -2;
+$operator = '/';
+if(($b == 0 || $a == 0) && ($operator == '/' || $operator == '%')){
     echo "На ноль делить нельзя";
+}else{
+    switch ($operator){
+        case "+":
+            echo $a + $b;
+            break;
+        case "-":
+            echo $a - $b;
+            break;
+        case "/":
+            echo $a / $b;
+            break;
+        case "%":
+            echo $a % $b;
+            break;
+    }
 }
-switch ($operator){
-    case "+":
-        echo $a + $b;
-        break;
-    case "-":
-        echo $a - $b;
-        break;
-    case "/":
-        echo $a / $b;
-        break;
-    case "%":
-        echo $a % $b;
-        break;
-}
+
 /**
  * Created by PhpStorm.
  * User: Андрей
