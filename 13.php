1079da7e0e0b8492f317fe13af60b9b675a6b65a
Andrew <andrey.albul@gmail.com>
2017-06-23 04:46:53 +0300

    Заданы две переменные: s - длина участка, который проехал автомобиль; t - время движения в часах. Вывести скорость автомобиля на заданном участке в км/час и в м/сек.

---
+++ 13.php
@@ -0,0 +1,12 @@
+<?php
+$s = 240;
+$t = 3;
+$rez_km = $s / $t;
+$rez_sec = $s / ($t*3.6);
+echo $rez_km . " км/ч" . "<br>" . $rez_sec . " м/с";
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 23.06.2017
+ * Time: 4:29
+ */
