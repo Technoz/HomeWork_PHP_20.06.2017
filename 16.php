b8a21e5b6471c476cdfb1b84c7368372fa1c8647
Andrew <andrey.albul@gmail.com>
2017-06-23 05:12:04 +0300

    16. Создайте две переменные $a и $b, значения которых будут числами. Выведите максимальное число из этих двух

---
+++ 16.php
@@ -0,0 +1,16 @@
+<?php
+$a = -110;
+$b = 120;
+if($a > $b){
+    echo $a;
+} elseif ($a == $b){
+    echo "Они равны";
+}   else{
+    echo $b;
+}
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 23.06.2017
+ * Time: 5:07
+ */
